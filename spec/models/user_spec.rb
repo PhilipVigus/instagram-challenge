564f1fb13761b7028fb17095296c47be1e292b69
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-11 13:31:03 +0100

    Add test for password too short on signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'is valid with valid attributes' do
-    user = User.new(username: 'dracula', email: 'drac@trans.com', password: '1234')
+    user = User.new(username: 'dracula', email: 'drac@trans.com', password: '123456789')
     expect(user).to be_valid
   end
 
   it 'is invalid without a username' do
-    user = User.new(username: nil, email: 'drac@trans.com', password: '1234')
+    user = User.new(username: nil, email: 'drac@trans.com', password: '123456789')
     expect(user).to_not be_valid
   end
 
   it 'is invalid without an email' do
-    user = User.new(username: 'dracula', email: nil, password: '1234')
+    user = User.new(username: 'dracula', email: nil, password: '123456789')
     expect(user).to_not be_valid
   end
 
@@ -21,15 +21,20 @@ RSpec.describe User, type: :model do
     expect(user).to_not be_valid
   end
 
+  it 'is invalid if the password is too short' do
+    user = User.new(username: 'dracula', email: 'drac@trans.com', password: '1234')
+    expect(user).to_not be_valid
+  end
+
   it 'is invalid with a duplicate username' do
-    User.new(username: 'dracula', email: 'drac@trans.com', password: '1234').save
-    user = User.new(username: 'dracula', email: 'drac@bats.com', password: '1234')
+    User.new(username: 'dracula', email: 'drac@trans.com', password: '123456789').save
+    user = User.new(username: 'dracula', email: 'drac@bats.com', password: '123456789')
     expect(user).to_not be_valid
   end
 
   it 'is invalid with a duplicate email' do
-    User.new(username: 'dracula', email: 'drac@trans.com', password: '1234').save
-    user = User.new(username: 'batty', email: 'drac@trans.com', password: '1234')
+    User.new(username: 'dracula', email: 'drac@trans.com', password: '123456789').save
+    user = User.new(username: 'batty', email: 'drac@trans.com', password: '123456789')
     expect(user).to_not be_valid
   end
 end
