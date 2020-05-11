cb106bda1d8ab23976f8d19072804089b06a7b3e
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-11 08:40:41 +0100

    Pass test for signing up successfully

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.email.downcase!
+
+    @user.save
+    redirect_to new_user_path, notice: 'Successfully signed up, please log-in'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe 'GET /new ' do
+    it 'responds with 200' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST /' do
+    it 'creates a user' do
+      post :create, params: { user: { username: 'dracula', email: 'drac@trans.com', password: '1234' } }
+      expect(User.find_by(username: 'dracula')).to be
+    end
+  end
+end
