a17fe1de8290173f6a94e1937e0f98f79896e6af
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-12 07:05:39 +0100

    Add test for unauthorised route access when not logged in

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -1,16 +1,24 @@
 require 'rails_helper'
+require 'login_helper'
 
 RSpec.describe PicturesController, type: :controller do
   describe "GET /new " do
-    it "responds with 200" do
+    it "responds with 200 when you are logged in" do
+      login
       get :new
       expect(response).to have_http_status(200)
+    end
+
+    it "redirects back to login when you are not logged in" do
+      get :new
+      expect(response).to redirect_to('/login')
     end
   end
 
   describe "POST /" do
     context "correct params" do
       before do
+        login
         file = fixture_file_upload(Rails.root.join('spec/files/picture1.png'), 'image/png')
         post :create, params: { picture: { picture: file } }
       end
@@ -24,10 +32,18 @@ RSpec.describe PicturesController, type: :controller do
       end
     end
 
-    context "missing params" do
+    context "incorrect params" do
       it "redirects back to pictures/new when a picture param is not specified" do
+        login
         post :create, params: nil
         expect(response).to redirect_to(new_picture_path)
+      end
+    end
+
+    context "unauthorised access" do
+      it "redirects back to login when you are not logged in" do
+        post :create, params: nil
+        expect(response).to redirect_to('/login')
       end
     end
   end
