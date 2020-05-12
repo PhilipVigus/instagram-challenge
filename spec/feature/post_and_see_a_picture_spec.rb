d9ebf142f29e6e16637e9802de3ef00d0c70c8a3
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-12 13:27:12 +0100

    Refactor posting pictures feature tests

--- spec/feature/post_and_see_a_picture_spec.rb
+++ spec/feature/post_and_see_a_picture_spec.rb
@@ -2,36 +2,27 @@ require 'rails_helper'
 require 'login_helper'
 
 RSpec.feature 'posting pictures', type: :feature do
-  scenario 'choose a picture to upload, see the picture on its page' do
+  before do
     User.create(username: 'dracula', email: 'drac@trans.com', password: 'teetharepointy')
     visit '/login'
     fill_in 'Username', with: 'dracula'
     fill_in 'Password', with: 'teetharepointy'
     click_on 'Submit'
     click_on 'Post a new picture'
+  end
+
+  scenario 'choose a picture to upload, see the picture on its page' do
     attach_file('picture', 'spec/files/picture1.png')
     click_on 'Submit'
     expect(page).to have_selector "img[src*=picture1]"
   end
 
   scenario 'dont choose a picture, get told to choose a picture' do
-    User.create(username: 'dracula', email: 'drac@trans.com', password: 'teetharepointy')
-    visit '/login'
-    fill_in 'Username', with: 'dracula'
-    fill_in 'Password', with: 'teetharepointy'
-    click_on 'Submit'
-    click_on 'Post a new picture'
     click_on 'Submit'
     expect(page).to have_content 'You must choose a picture to upload'
   end
 
   scenario 'post a picture with a caption' do
-    User.create(username: 'dracula', email: 'drac@trans.com', password: 'teetharepointy')
-    visit '/login'
-    fill_in 'Username', with: 'dracula'
-    fill_in 'Password', with: 'teetharepointy'
-    click_on 'Submit'
-    click_on 'Post a new picture'
     attach_file('picture', 'spec/files/picture1.png')
     fill_in 'picture[caption]', with: 'An oh so hilarious picture of my favourite pet, batty the bat'
     click_on 'Submit'
