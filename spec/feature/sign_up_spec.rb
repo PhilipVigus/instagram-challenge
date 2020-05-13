9796cb3c917af055cb736ac37ce52dad4448a12a
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-13 06:24:58 +0100

    Add userfriendly route name for signup page

--- spec/feature/sign_up_spec.rb
+++ spec/feature/sign_up_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'signing up to site', type: :feature do
   scenario 'successfully sign up to the site' do
-    visit '/users/new'
+    visit signup_path
     fill_in 'Username', with: 'dracula'
     fill_in 'Email', with: 'drac@trans.com'
     fill_in 'Password', with: 'teetharepointy'
@@ -12,7 +12,7 @@ RSpec.feature 'signing up to site', type: :feature do
 
   scenario 'unsuccessfully sign up to the site' do
     User.create(username: 'dracula', email: 'drac@trans.com', password: 'teetharepointy')
-    visit '/users/new'
+    visit signup_path
     fill_in 'Username', with: 'dracula'
     fill_in 'Email', with: 'drac@trans.com'
     fill_in 'Password', with: 'teetharepointy'
