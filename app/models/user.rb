bc0796d7c24c17b849647e24c23155b72e9221dc
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-13 05:36:45 +0100

    Add has many association  from user to pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 8..20 }
+  has_many :pictures
 end
