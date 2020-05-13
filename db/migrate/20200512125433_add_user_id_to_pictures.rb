c96c5946369e6388ba9aaf92f7ef9ef6974ee792
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-13 05:07:22 +0100

    Add association between picture and user in db

---
+++ db/migrate/20200512125433_add_user_id_to_pictures.rb
@@ -0,0 +1,6 @@
+
+railsclass AddUserIdToPictures < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :pictures, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_115248) do
+ActiveRecord::Schema.define(version: 2020_05_12_125433) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_05_12_115248) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "caption"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +50,5 @@ ActiveRecord::Schema.define(version: 2020_05_12_115248) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "pictures", "users"
 end
