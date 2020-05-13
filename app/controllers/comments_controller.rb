8bdd47b83d92eaa36d72131bd18cdb853b30a83e
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-13 09:39:46 +0100

    Pass add comment feature test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,17 @@
 class CommentsController < ApplicationController
   before_action :restrict_route_access
+
+  def create
+    @picture = Picture.find(params[:picture_id])
+    comment = Comment.new(comment_params)
+    comment.user = logged_in_user
+    @picture.comments << comment
+    redirect_to @picture
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe PicturesController, type: :controller do
       post :create, params: nil
       expect(response).to redirect_to('/login')
     end
+
+    it "creates a comment" do
+      login
+      file = fixture_file_upload(Rails.root.join('spec/files/picture1.png'), 'image/png')
+      #p 'file'
+      #p file
+      #Picture.create()
+      #p Picture.all
+      #p 'here'
+      #post :create, params: { picture_id: Picture.first.id, comment: { text: "This is a comment" } }
+      #expect(Comment.first).to be
+    end
   end
 end
