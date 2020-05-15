b99f9fd6c91dac02f457a64f0b4532f9fdb6773b
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-15 13:57:02 +0100

    Complete implementation of comments on pictures
    The implementaiton is complete. However, the comment controller
    test for creating a picture is broken and has been disabled.
    
    The reason I've committed here is that I've realised there's a
    possible refactor I can do that will massively simplify the
    writing of the test that I'm struggling with, so I want to explore
    that separately.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@ class CommentsController < ApplicationController
   before_action :restrict_route_access
 
   def create
-    @picture = Picture.find(params[:picture_id])
     comment = Comment.new(comment_params)
     comment.user = logged_in_user
+    @picture = Picture.find(params[:picture_id])
     @picture.comments << comment
     redirect_to @picture
   end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,23 +1,24 @@
 require 'rails_helper'
 require 'login_helper'
 
-RSpec.describe PicturesController, type: :controller do
+RSpec.describe CommentsController, type: :controller do
   describe "POST /" do
     it "redirects back to login when you are not logged in" do
-      post :create, params: nil
+      # it doesn't matter what id's are used here.
+      # they just need to be specified for the post route
+      # to work at all
+      post :create, params: { user_id: 1, picture_id: 1}
       expect(response).to redirect_to('/login')
     end
 
-    it "creates a comment" do
+    xit "creates a comment" do
       login
       file = fixture_file_upload(Rails.root.join('spec/files/picture1.png'), 'image/png')
-      #p 'file'
-      #p file
-      #Picture.create()
-      #p Picture.all
-      #p 'here'
-      #post :create, params: { picture_id: Picture.first.id, comment: { text: "This is a comment" } }
-      #expect(Comment.first).to be
+      p 'file'
+      p file
+      picture = Picture.create(picture: file)
+      p picture
+      expect(true).to eq false
     end
   end
 end
