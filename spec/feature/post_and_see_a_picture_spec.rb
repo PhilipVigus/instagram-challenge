28eeec64f4fd3eb92acba3ab8f5d21bc6bacc50c
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-16 16:14:03 +0100

    Remove duplicate test

--- spec/feature/post_and_see_a_picture_spec.rb
+++ spec/feature/post_and_see_a_picture_spec.rb
@@ -35,12 +35,4 @@ RSpec.feature 'posting pictures', type: :feature do
     click_on 'Submit'
     expect(page).to have_content "Posted by dracula"
   end
-
-  scenario 'dont see a picture if you didnt post it and you havent liked it' do
-    attach_file('picture', 'spec/files/picture1.png')
-    click_on 'Submit'
-    create_user_and_login('batty', 'bat@cave.com', 'flyflyfly')
-    visit "/users/#{User.last.id}"
-    expect(page).to_not have_content "Posted by dracula"
-  end
 end
