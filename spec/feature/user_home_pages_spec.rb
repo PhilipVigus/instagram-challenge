1096620d5c8ae59268502c9b34ef06d55dbb6038
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-16 15:50:23 +0100

    Add additional test to check own posts are seen on homepage

--- spec/feature/user_home_pages_spec.rb
+++ spec/feature/user_home_pages_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 require 'login_helper'
 
 RSpec.feature 'user home pages', type: :feature do
-  scenario 'only a users own posts show on their homepage' do
+  before do
     create_user_and_login('dracula', 'drac@trans.com', 'teetharepointy')
     click_on 'Post a new picture'
     attach_file('picture', 'spec/files/picture1.png')
     click_on 'Submit'
+  end
+
+  scenario 'see your own pictures' do
+    visit "/users/#{User.last.id}"
+    expect(page).to have_selector "img[src*=picture1]"
+  end
+
+  scenario 'not see other user pictures' do
     create_user_and_login('batty', 'bat@cave.com', 'flyflyfly')
     visit "/users/#{User.last.id}"
     expect(page).to_not have_selector "img[src*=picture1]"
