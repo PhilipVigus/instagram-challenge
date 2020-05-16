a4ebe11f80e88b1d80a13bd266615e0815d4ac5d
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-16 16:13:01 +0100

    Pass test for not seeing others pictures on your homepage

--- spec/feature/post_and_see_a_picture_spec.rb
+++ spec/feature/post_and_see_a_picture_spec.rb
@@ -36,10 +36,11 @@ RSpec.feature 'posting pictures', type: :feature do
     expect(page).to have_content "Posted by dracula"
   end
 
-  xscenario 'dont see a picture if you didnt post it and you havent liked it' do
+  scenario 'dont see a picture if you didnt post it and you havent liked it' do
     attach_file('picture', 'spec/files/picture1.png')
     click_on 'Submit'
     create_user_and_login('batty', 'bat@cave.com', 'flyflyfly')
+    visit "/users/#{User.last.id}"
     expect(page).to_not have_content "Posted by dracula"
   end
 end
