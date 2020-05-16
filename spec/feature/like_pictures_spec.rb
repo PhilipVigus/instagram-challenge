7e9675e809c38c7af438597ad4d8092111e55238
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-16 14:45:28 +0100

    Add and pass test for unliking a picture

--- spec/feature/like_pictures_spec.rb
+++ spec/feature/like_pictures_spec.rb
@@ -12,4 +12,16 @@ RSpec.feature 'like pictures', type: :feature do
     click_on "like_picture"
     expect(page).to have_button '1 like'
   end
+
+  scenario 'unlike a picture' do
+    create_user_and_login('dracula', 'drac@trans.com', 'teetharepointy')
+    click_on 'Post a new picture'
+    attach_file('picture', 'spec/files/picture1.png')
+    click_on 'Submit'
+    create_user_and_login('batty', 'bat@cave.com', 'flyflyfly')
+    click_link(Picture.first.id)
+    click_on "like_picture"
+    click_on "like_picture"
+    expect(page).to have_button '0 likes'
+  end
 end
