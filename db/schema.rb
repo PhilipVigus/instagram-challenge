f46d4b17d80211fe673eafc200340e3c225048ff
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-07 12:08:04 +0100

    Fix bug in db migration file

--- db/schema.rb
+++ db/schema.rb
@@ -52,9 +52,9 @@ ActiveRecord::Schema.define(version: 2020_05_13_065915) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "email", null: false
-    t.string "password_digest", null: false
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
