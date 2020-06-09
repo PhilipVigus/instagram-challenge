027ac8e248d4d02cd354b60aed66222314a2f181
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-09 16:26:11 +0100

    Disable user handles feature test for now

--- spec/feature/user_handles_spec.rb
+++ spec/feature/user_handles_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'login_helper'
 
 RSpec.feature 'user handles', type: :feature do
-  scenario 'click on a handle and go to the user homepage' do
+  xscenario 'click on a handle and go to the user homepage' do
     create_user_and_login('dracula', 'drac@trans.com', 'teetharepointy')
     click_on 'Post a new picture'
     attach_file('picture', 'spec/files/picture1.png')
